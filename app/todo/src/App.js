27ddd452dc065d601a25a921a9895508768c0605
yyamada12 <12yacropolisy@gmail.com>
2019-05-15 08:26:37 +0900

    [add] style of title with material-ui

--- app/todo/src/App.js
+++ app/todo/src/App.js
@@ -1,10 +1,25 @@
 import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 import TaskList from "./TaskList/TaskList";
 import AddTask from "./AddTask/AddTask";
 import Title from "./Title/Title";
 
-export default class App extends React.Component {
+const styles = theme => ({
+  layout: {
+    width: "auto",
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    [theme.breakpoints.up(1100 + theme.spacing.unit * 3 * 2)]: {
+      width: 1100,
+      marginLeft: "auto",
+      marginRight: "auto"
+    }
+  }
+});
+
+class App extends React.Component {
   constructor() {
     super();
 
@@ -59,23 +74,29 @@ export default class App extends React.Component {
   }
 
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        <Title
-          num_of_remains={this.state.todos.filter(todo => !todo.isDone).length}
-          num_of_todos={this.state.todos.length}
-        />
-        <TaskList
-          todos={this.state.todos}
-          removeTask={this.removeTask}
-          checkTodo={this.checkTodo}
-        />
-        <AddTask
-          addTask={this.addTask}
-          onChangeInput={this.onChangeInput}
-          input={this.state.input}
-        />
-      </div>
+      <React.Fragment>
+        <CssBaseline />
+        <div className={classes.layout}>
+          <Title
+            num_of_remains={
+              this.state.todos.filter(todo => !todo.isDone).length
+            }
+            num_of_todos={this.state.todos.length}
+          />
+          <TaskList
+            todos={this.state.todos}
+            removeTask={this.removeTask}
+            checkTodo={this.checkTodo}
+          />
+          <AddTask
+            addTask={this.addTask}
+            onChangeInput={this.onChangeInput}
+            input={this.state.input}
+          />
+        </div>
+      </React.Fragment>
     );
   }
 }
@@ -83,3 +104,5 @@ export default class App extends React.Component {
 function getUniqueId() {
   return new Date().getTime().toString(36) + "-" + Math.random().toString(36);
 }
+
+export default withStyles(styles)(App);
--- app/todo/src/Title/Title.js
+++ app/todo/src/Title/Title.js
@@ -1,20 +1,73 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
 
-const Title = props => (
-  <div>
-    <h1> Todo App </h1>
-    {props.num_of_remains && props.num_of_todos && (
-      <span data-testid="counts">
-        ({props.num_of_remains}/{props.num_of_todos})
-      </span>
-    )}
-  </div>
-);
+const styles = theme => ({
+  title: {
+    backgroundColor: theme.palette.grey[800],
+    color: theme.palette.common.white,
+    marginBottom: theme.spacing.unit * 4
+  },
+  titleContent: {
+    padding: `${theme.spacing.unit * 6}px`,
+    [theme.breakpoints.up("md")]: {
+      paddingRight: 0
+    }
+  },
+  counts: {
+    marginLeft: theme.spacing.unit * 2
+  }
+});
+
+const Title = props => {
+  const { classes } = props;
+
+  return (
+    <React.Fragment>
+      <Paper className={classes.title}>
+        <Grid container>
+          <Grid item md={6}>
+            <div className={classes.titleContent}>
+              <Typography
+                component="h1"
+                variant="h3"
+                color="inherit"
+                inline
+                gutterBottom
+              >
+                Todo App
+              </Typography>
+              {props.num_of_remains && props.num_of_todos ? (
+                <Typography
+                  className={classes.counts}
+                  variant="h5"
+                  color="inherit"
+                  inline
+                  gutterBottom
+                  data-testid="counts"
+                >
+                  ({props.num_of_remains}/{props.num_of_todos})
+                </Typography>
+              ) : null}
+
+              <Typography variant="h5" color="inherit" paragraph>
+                Welcome to my todo app!
+              </Typography>
+            </div>
+          </Grid>
+        </Grid>
+      </Paper>
+    </React.Fragment>
+  );
+};
 
 Title.propTypes = {
   num_of_remains: PropTypes.number,
-  num_of_todos: PropTypes.number
+  num_of_todos: PropTypes.number,
+  classes: PropTypes.object.isRequired
 };
 
-export default Title;
+export default withStyles(styles)(Title);
